6239a40d2670cd0f1100dc88e232084f771a61fb
Ekundayo Alabi <Eoa1903@hsutx.edu>
2022-06-07 00:04:03 -0500

    Initial

---
+++ src/main/java/com/dayo/bookreview/bookreview/BookreviewApplication.java
@@ -0,0 +1,13 @@
+package com.dayo.bookreview.bookreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookreviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dayo/bookreview/bookreview/BookreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayo.bookreview.bookreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
